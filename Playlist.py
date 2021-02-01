f2861ebe40f56b3b9a9a837dd04e909d3db9d72f
Tian Qiu <tian.qiu142@gmail.com>
2021-01-31 18:50:06 -0800

    added add_song function in Playlist

--- Playlist.py
+++ Playlist.py
@@ -1,44 +1,48 @@
 from Song import Song
 
+
 class Playlist:
-  def __init__(self):
-    self.__first_song = None
+    def __init__(self):
+        self.__first_song = None
 
+    # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
 
-  # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
+    def add_song(self, title):
+        new_song = Song(title)
 
-  def add_song(self, title):
-    pass
+        if self.__first_song = None:
+            self.__first_song = new_song
+            return
 
+        # else traverse till last song
+        last_song = self.__first_song
+        while(last_song.__next_song):
+            last_song = last_song.next
 
+        # Change the next of the last song
+        last_song.next = new_song
 
-  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
+    # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
-  def find_song(self, title):
-    pass
+    def find_song(self, title):
+        pass
 
+    # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed.
 
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
+    def remove_song(self, title):
+        pass
 
-  def remove_song(self, title):
-    pass
+    # TODO: Create a method called length, which returns the number of songs in the playlist.
 
+    def length(self):
+        pass
 
+    # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
 
-  # TODO: Create a method called length, which returns the number of songs in the playlist.
+    # Example:
+    # 1. Song Title 1
+    # 2. Song Title 2
+    # 3. Song Title 3
 
-  def length(self):
-    pass
-
-
-  # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
-
-  # Example:
-  # 1. Song Title 1
-  # 2. Song Title 2
-  # 3. Song Title 3
-
-  def print_songs(self):
-    pass
-
-  
+    def print_songs(self):
+        pass
